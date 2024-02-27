1a6ba4672644c08c7e5dd5346b933f5405530d97
7ketu <101.mozilla.s.s@gmail.com>
2024-02-27 21:07:55 +0530

    first commit

---
+++ api/main.py
@@ -0,0 +1,86 @@
+# from fastapi import FastAPI
+
+# import uvicorn
+
+# app = FastAPI()
+
+# @app.get("/ping")
+# async def ping():
+#     return "hello"
+
+# if __name__=="__main__":
+#     uvicorn.run(app, host='localhost', port=8000)
+
+
+
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+import cv2
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+MODEL = tf.keras.models.load_model("../saved_models/2")
+
+CLASS_NAMES = [ 'Potato___Early_blight',
+ 'Potato___Late_blight',
+ 'Potato___healthy']
+# 'Pepper__bell___Bacterial_spot',
+#  'Pepper__bell___healthy',
+
+#  'Tomato_Early_blight',
+#  'Tomato_Late_blight',
+#  'Tomato__Tomato_YellowLeaf__Curl_Virus',
+#  'Tomato__Tomato_mosaic_virus',
+#  'Tomato_healthy',
+#  'bees',
+#  'weevil']
+
+@app.get("/ping")
+async def ping():
+    return "Hello, I am alive"
+
+# def read_file_as_image(data) -> np.ndarray:
+#     image = np.array(Image.open(BytesIO(data)))
+#     return image
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    resized_image = cv2.resize(image, (256, 256))  # Resize the image to match the model's expected input shape
+    return resized_image
+
+
+@app.post("/predict")
+async def predict(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+    
+    predictions = MODEL.predict(img_batch)
+
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8000)
